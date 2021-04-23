9d3416af23e6272933983c18f668e6b6eced1ff3
Tiago <tiago.melres@gmail.com>
2021-04-23 10:52:22 +0100

    minor_corrections

--- R crash course_Code/R crash course_Code.R
+++ R crash course_Code/R crash course_Code.R
@@ -8,6 +8,8 @@ print("Olá!")
 getwd()
 setwd("C:/Users/tiago/Desktop")
 
+choose.dir()
+
 # Operadores e objetos 
 x1 <- 10 # assigns number 10 to object "x1"
 x1 # display object "x1""
@@ -43,6 +45,9 @@ rm(x1)  # remove "x1" from the environment
 rm(list = ls())  # using ls() in the argument "list" will 
                  # remove all objects from the environment
 
+rm(list=ls())
+
+
 ## Limpar consola
 cat("\014") # cleans console 
 # OR 
@@ -51,7 +56,10 @@ shell("cls") # shell("clear") if Linux or Mac.
 
 ## Ajuda  
 help.start() # general help
-help("rm") # help on the function "rm" (quotation marks are optional) | OR ?rm
+
+help("rm") # help on the function "rm" (quotation marks are optional) | OR 
+
+?mean
 help.search("ls") # searches the help system using the keyword "ls" | OR ??ls
 
 
@@ -59,7 +67,19 @@ help.search("ls") # searches the help system using the keyword "ls" | OR ??ls
 ## Estruturas de dados no R
 id <- c(1:5)
 sex <- c("Male", "Male", "Female", "Male", "Female")
-t0_date <- as.Date(c('2017-11-2','2017-11-4','2017-11-5','2017-11-10','2017-11-11'))
+class(sex)
+
+name <- c("Ana", "Manuel", "Afonso", "Maria") # character vector
+name
+
+t0_date <- as.Date(c("2017-11-2",'2017-11-4','2017-11-5','2017-11-10','2017-11-11'))
+
+ruser <- c(TRUE, FALSE, FALSE, TRUE) 
+class(ruser)
+class(name)
+
+length(ruser)
+
 t0_beh_prob <- c(11, 14, 5, 20, 16)
 intervention <- c(TRUE, TRUE, FALSE, TRUE, FALSE)
 t1_date <- as.Date(c('2018-11-1','2018-11-3','2018-11-4','2018-11-10','2018-11-11'))
@@ -82,11 +102,28 @@ class(ruser)
 table(ruser)
 rbaseskills <- c(40, 35, 20, 60) # previous skills in R using a scale from 1 to 100
 rmotivation <- c(60, 90, 10, 50) # motivation in a scale from 1 to 100
+
+
+
+
+
+
 rbaseskills[2] # returns vector's 2nd value
-rbaseskills[c(2,3)] # returns vector's 2nd and 3rd values
+
+ruser
+rbaseskills[ruser] # returns vector's 2nd and 3rd values
+
+
+
+des <- c(100, 102, 90, 50)
+
+
+
 rbaseskills > rmotivation # logical operations
 rbaseskills + 1 # arithmetic operations
 rbaseskills + rmotivation # arithmetic operations
+
+
 
 ## Estruturas de dados - Matrizes  
 dailyuse <- rnorm(20, mean = 2, sd = .5) # Creates a vector with 20 elements
@@ -100,6 +137,7 @@ class(dailyuse)
 dailyuse[c(1:3), 2]  # select rows from 1 to 3 and columns 2
 dailyuse[c(1, 4), ] # leaving column field blank will select entire columns
 dailyuse[ -2:-3, -1] # select all rows 2 and 3 and columns 1
+
 a <- "one" # Creates a character vector
 b <- 1 # Creates a numeric vector
 c <- 1L # Creates a numeric (integer) vector
@@ -108,6 +146,7 @@ print(c(a, class(a)))
 print(c(b, class(b)))
 print(c(c, class(c)))
 print(c(d, class(d)))
+
 
 ## Classes de objetos e coerção (cont.)   
 a <- c(1.4, "apple", 1, 6.3, "fig", 2, TRUE) 
@@ -123,29 +162,46 @@ class(c)
 ## Estruturas de dados - Bases de dados   
 df  <-  data.frame(id, name, ruser, rbaseskills, rmotivation, dailyuse) 
 df
+
 data() # lists all available datasets
 browseURL("http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/00Index.html") 
 # informations about available datasets
 ?mtcars # diplays information about cars dataset
 data(mtcars)  # loading a dataset
+
 dim(mtcars)
 names(mtcars)
 ncol(mtcars)
 nrow(mtcars)
+
 str(mtcars)
 
 mtcars # displays a dataset
 head(mtcars,2)
+
 mtcars[1:3, 1]
+
 mtcars$mpg
+
+
+sample(1:nrow(mtcars), 2)
 mtcars[sample(1:nrow(mtcars), 2), ]
+
+
 
 formals(read.table) # the list of function's arguments
 ?read.table
 ?formals
+
+
+
+getwd
+
+choose.files()
+
 bechdel <-
   read.table(
-    "G:\\My Drive\\FPCEUP\\R trainning\\GitRepo\\R crash course\\R_crash_course\\R crash course-Data\\bechdel.csv",
+    "G:\\My Drive\\FPCEUP\\R trainning\\GitRepo\\R crash course\\R_crash_course\\R crash course_Data\\bechdel.csv",
     header = TRUE,
     sep = ";",
     quote = "\"", 
@@ -160,26 +216,39 @@ str(bechdel)
 
 bechdel <-
   read.csv2(
-    "G:\\My Drive\\FPCEUP\\R trainning\\GitRepo\\R crash course\\R_crash_course\\R crash course-Data\\bechdel.csv",
+    "G:\\My Drive\\FPCEUP\\R trainning\\GitRepo\\R crash course\\R_crash_course\\R crash course_Data\\bechdel.csv",
     fileEncoding="UTF-8-BOM")
 head(bechdel, 2)
 dim(bechdel)
 names(bechdel)
 summary(bechdel)
 
-#install.packages("foreign", "Hmisc")
+
+install.packages("foreign", "Hmisc")
+install.packages
+
+
+
 library(foreign)
 library(Hmisc)
 tail(ls("package:Hmisc"))
 ?read.spss
-bechdel <- read.spss("G:\\My Drive\\FPCEUP\\R trainning\\GitRepo\\R crash course\\R_crash_course\\R crash course-Data\\bechdel.sav", 
+
+bechdel <- read.spss("G:\\My Drive\\FPCEUP\\R trainning\\GitRepo\\R crash course\\R_crash_course\\R crash course_Data\\bechdel.sav", 
                      to.data.frame = TRUE)
+
 str(bechdel)
 dim(bechdel)
 bechdel[1234, ]
-bechdel <- spss.get("G:\\My Drive\\FPCEUP\\R trainning\\GitRepo\\R crash course\\R_crash_course\\R crash course-Data\\bechdel.sav")
+
+?spss.get
+
+bechdel <- spss.get("G:\\My Drive\\FPCEUP\\R trainning\\GitRepo\\R crash course\\R_crash_course\\R crash course_Data\\bechdel.sav")
+
+
 str(bechdel)
 head(bechdel, 3)
+
 
 # Funções integradas (built-in) para procedimentos estatísticos básicos
 
